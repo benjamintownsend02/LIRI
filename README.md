32442aeca7db95a5634614e8edc0f4d73d2e5b72
Benjamin Townsend <benjamin.townsend.04@gmail.com>
2020-04-02 08:04:18 -0700

    added initial files

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # LIRI
-An app for searching various data.
+LIRI is a Node.js CLI app that allows the user to search for various information readouts from a single source, saving time. It is run from liri.js.
+## Instructions
+When running the app, the user's first command line argument is taken as the type of search, and all subsequent arguments are taken as the text of the search. 
+Valid search types are:
+1. "spotify-this-song" : searches using Spotify for a song name and logs artist, song name, preview url, and album name.
+Example:
+![alt text](screenshots/spotify.png)
+
+2.  "concert-this": searches using the Bands in Town Artist Events API for an artist and logs the venue name, location, and event date of the next event. 
+Example:
+![alt text](screenshots/concert.png)
+
+3. "movie-this" : searches using the OMBD API for a movie title and logs the movie's title, year, IMDB rating, Rotten Tomatoes rating, country, language, plot summary, and actor list.
+Example:
+![alt text](screenshots/movie.png)
+
+4. "do-what-it-says" : reads the random.txt file and takes the string values before and after the first comma as the search type and search term, then logs the appropriate results (i.e. it performs one of the search types 1-3).
+Example:
+![alt text](screenshots/do-what.png)
+
+Failure to enter a search term results in results being returned for a default term (e.g. the singer celine dion).
+
+## Dependencies
+LIRI requires the following node modules:
+1. axios
+2. dotenv
+3. moment
+4. node-spotify-api
+Please see the package.json file for more information.
+
+Additionally, the user must currently provide a .env file with valid spotify ID and secret keys for the api to function.
+
+## Other Information
+Version: 1.0
+
+Planned Improvements: .txt file output logging to log.txt (currently unused)
+
+Author: benjamintownsend02
+
+Known Issues: none
---
+++ liri.js
@@ -0,0 +1,136 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var fs = require('fs');
+var moment = require('moment');
+var axios = require('axios');
+var Spotify=require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+
+var searchType=process.argv[2];
+var searchTerm=process.argv.slice(3).join(" ");
+
+
+function search(searchType, SearchTerm){
+    if(searchType==="concert-this")
+    {
+        var artist="celine+dion";
+        if(searchTerm.length>0)
+        {
+            artist=searchTerm.replace(" ","+");
+        }
+        var bandURL="https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+        axios.get(bandURL).then(
+        function(response) {
+        var result = response.data[0];
+        console.log("Venue Name: " + result.venue.name);
+        console.log("Venue Location: " + result.venue.city + ", " + result.venue.country);
+        console.log("Event Date: " + moment(result.datetime).format("MM/DD/YYYY"));
+        })
+        .catch(function(error) {
+        if (error.response) {
+          console.log("---------------Data---------------");
+          console.log(error.response.data);
+          console.log("---------------Status---------------");
+          console.log(error.response.status);
+          console.log("---------------Status---------------");
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+    }
+    else if(searchType==="spotify-this-song")
+    {
+        var song="The+Sign";
+        if(searchTerm.length>0)
+        {
+            song=searchTerm.replace(" ","+");
+        }
+        "https://api.spotify.com/v1/search?q=" + song
+         var spotifyURL="https://api.spotify.com/v1/search?q=" + song + "/events?app_id=codingbootcamp"
+        spotify.search({type: "track", query: song, limit: 1}, function(err,data){
+        if(err){
+            return console.log("Error: " + err)
+        }
+        var result=data.tracks.items[0];
+        var artistString="";
+        for(var i=0; i<result.artists.length; i++)
+        {
+            artistString = artistString + result.artists[i].name + ", ";
+        }
+        artistString.substring(0,artistString.length-2);
+        console.log("Artist(s): " + artistString);
+        console.log("Song Name: " + result.name);
+        console.log("Preview URL: " + result.preview_url);
+        console.log("Album: " + result.album.name);
+        });
+    }
+    else if(searchType==="movie-this")
+    {
+        var forcedYear="2009"
+        var movieName="mr+nobody";
+        if(searchTerm.length>0)
+        {
+            movieName=searchTerm.replace(" ","+");
+            forcedYear="";
+        }
+        var movieURL="http://www.omdbapi.com/?t=" + movieName + "&y=" + forcedYear + "&plot=short&apikey=trilogy";
+        axios.get(movieURL).then(
+        function(response) {
+        var result=response.data;
+        console.log("Title: " + result.Title);
+        console.log("Release Year: " + result.Year);
+        console.log("IMDB Rating: " + result.imdbRating);
+        var tomatoString="N/A";
+        for(var i=0; i<result.Ratings.length; i++)
+        {
+            if(result.Ratings[i].Source==='Rotten Tomatoes')
+            {
+                tomatoString=result.Ratings[i].Value;
+            }
+        }
+        console.log("Rotten Tomatoes Rating: " + tomatoString);
+        console.log("Country: " + result.Country);
+        console.log("Language: " + result.Language);
+        console.log("Plot: " + result.Plot);
+        console.log("Actors: " + result.Actors);
+    })
+    .catch(function(error) {
+    if (error.response) {
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+    });
+  }
+}
+
+if(searchType==="do-what-it-says")
+{
+    fs.readFile("random.txt","utf-8",function(err,data){
+        if (err) {
+            wait=false;
+            return console.log(err);
+        }
+        searchType = data.substring(0,data.indexOf(','));
+        searchTerm = data.substring(data.indexOf(',')+2,data.length-1);
+        search(searchType, searchTerm);
+    });
+}
+else
+{
+    search(searchType, searchTerm);
+}
+
